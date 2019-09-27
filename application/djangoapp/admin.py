4eef9e198c069bd1b4ee48a45f63d2a2b3e13dd0
Rudy Öcalan <rudy.ocalan@epita.fr>
2019-09-27 03:09:34 +0200

    [ADMIN] Register Produit and Customer model
    I saw this file and replaced its content by correct values, but I still don't know what it is used for.

--- application/djangoapp/admin.py
+++ application/djangoapp/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Article)
-admin.site.register(models.Vente)
+admin.site.register(models.Produit)
+admin.site.register(models.Customer)

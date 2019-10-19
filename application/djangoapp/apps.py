8f8d4add0d1195896f1da8120df78477f352287a
Rudy Öcalan <rudy.ocalan@epita.fr>
2019-10-19 20:09:58 +0200

    [APP] move app register and tasks remove task in dev environment in apps.py

--- application/djangoapp/apps.py
+++ application/djangoapp/apps.py
@@ -1,17 +1,27 @@
 import os
 
 from django.apps import AppConfig
+from apipkg import api_manager as api
+from django.views.decorators.csrf import csrf_exempt
+
+myappurl = "http://localhost:" + os.environ["WEBSERVER_PORT"]
 
 
 class ApplicationConfig(AppConfig):
     name = 'application.djangoapp'
 
     def ready(self):
-        if os.environ.get('RUN_MAIN') == 'true':
+        if os.environ.get('RUN_MAIN'):
+            api.unregister(os.environ['DJANGO_APP_NAME'])
+            api.register(myappurl, os.environ['DJANGO_APP_NAME'])
+
             from .models import GlobalInfo
             global_info = GlobalInfo.objects.first()
             if not global_info:
                 GlobalInfo().save()
+
+            if os.environ['ENV'] == 'dev':
+                api.post_request(host='scheduler', url='/app/delete?source=gestion-magasin', body={})
 
             from .views import schedule_task_simple
             schedule_task_simple('Magasin: Update Products', '/products/update/', 'day')
